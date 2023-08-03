39cbc7feedd860a1b7643e969ee0ef370af0dc78
unknown <bimalin733@gmail.com>
2023-08-03 14:32:03 +0530

    Your commit message here

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['google', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react'],
+  rules: {
+    '@next/next/no-img-element': 'off',
+  },
+};
