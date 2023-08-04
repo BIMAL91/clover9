7ec1897c09a6571ab74edb3344bcfc04874e0d0d
unknown <bimalin733@gmail.com>
2023-08-04 13:00:39 +0530

    first commit

--- pages/api/Display.js
+++ pages/api/Display.js
@@ -1,12 +1,14 @@
 // pages/api/Display.js
 
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'kismat'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 export default function handler(req, res) {
